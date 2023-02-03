ebce6902108a3aac9818d37058a8413a1d41f55f
RZ-Music <ryanzhu2005@gmail.com>
2023-02-03 09:57:00 -0500

    download json file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import { useState } from "react";
+import { ImDownload2 } from "react-icons/im";
 
 export default function Home() {
   const [username, setUsername] = useState("");
@@ -30,6 +31,17 @@ export default function Home() {
       .catch((err) => setError(err));
   };
 
+  const download = () => {
+    if (res == null) return;
+    const a = document.createElement("a");
+    const file = new Blob([JSON.stringify(res, null, 2)], {
+      type: "text/plain",
+    });
+    a.href = URL.createObjectURL(file);
+    a.download = `${username}_marks.json`;
+    a.click();
+  };
+
   return (
     <>
       <Head>
@@ -38,7 +50,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="w-screen min-h-screen flex flex-col items-center py-32 px-10 md:px-32 lg:px-60 bg-[url('/background.png')] bg-cover bg-no-repeat bg-center">
+      <main className="w-screen min-h-screen flex flex-col items-center pt-32 px-10 md:px-32 lg:px-60 bg-[url('/background.png')] bg-cover bg-no-repeat bg-center">
         <header className="w-full flex flex-col items-center">
           <h1 className="text-7xl font-black text-white">TA-API</h1>
           <p className="text-lg font-normal text-white mt-5">
@@ -49,7 +61,7 @@ export default function Home() {
             Used in the TeachAssist{" "}
             <a
               href="https://teachassistapp.github.io/"
-              className="underline font-bold"
+              className="underline font-bold hover:drop-shadow-[0px_0px_10px_rgba(219,231,219,0.7)] duration-300 ease-in-out hover:tracking-wider"
               rel="noreferrer noopener"
             >
               mobile app
@@ -61,17 +73,17 @@ export default function Home() {
               type="text"
               placeholder="Username"
               onChange={(e) => setUsername(e.target.value)}
-              className="bg-green-transparent text-white focus:outline-none w-1/3 px-6 py-3 placeholder-white-transparent rounded-full ring-2 ring-green-light shadow-[0px_0px_8px_3px_rgba(219,231,219,0.6)]"
+              className="bg-green-transparent text-white focus:outline-none w-1/3 px-6 py-3 placeholder-white-transparent rounded-full ring-2 ring-green-light shadow-[0px_0px_8px_3px_rgba(219,231,219,0.6)] duration-300 ease-in-out hover:shadow-[0px_0px_20px_3px_rgba(219,231,219,0.7)]"
             />
             <input
               type="password"
               placeholder="Password"
               onChange={(e) => setPassword(e.target.value)}
-              className="bg-green-transparent text-white focus:outline-none w-1/3 px-6 py-3 placeholder-white-transparent rounded-full ring-2 ring-green-light shadow-[0px_0px_8px_3px_rgba(219,231,219,0.6)]"
+              className="bg-green-transparent text-white focus:outline-none w-1/3 px-6 py-3 placeholder-white-transparent rounded-full ring-2 ring-green-light shadow-[0px_0px_8px_3px_rgba(219,231,219,0.6)] duration-300 ease-in-out hover:shadow-[0px_0px_20px_3px_rgba(219,231,219,0.7)]"
             />
             <button
               onClick={request}
-              className="bg-green-light w-1/4 text-green-dark text-center font-bold px-6 py-3 rounded-full ring-2 ring-green-light shadow-[0px_0px_8px_3px_rgba(219,231,219,0.6)]"
+              className="bg-green-light w-1/4 text-green-dark text-center font-bold px-6 py-3 rounded-full ring-2 ring-green-light shadow-[0px_0px_8px_3px_rgba(219,231,219,0.6)] hover:scale-105 duration-300 ease-in-out hover:shadow-[0px_0px_20px_3px_rgba(219,231,219,0.7)]"
             >
               Get Marks!
             </button>
@@ -87,6 +99,13 @@ export default function Home() {
                 </pre>
               </div>
             </div>
+            <button
+              onClick={download}
+              className="bg-green-light text-green-dark text-center font-bold mt-5 px-6 py-3 rounded-full ring-2 ring-green-light shadow-[0px_0px_8px_3px_rgba(219,231,219,0.6)] hover:scale-105 duration-300 ease-in-out hover:shadow-[0px_0px_20px_3px_rgba(219,231,219,0.7)]"
+            >
+              Download JSON
+              <ImDownload2 className="inline-block ml-2" />
+            </button>
           </section>
         )}
         <section className="w-full flex flex-col items-center mt-20">
@@ -99,7 +118,7 @@ export default function Home() {
           </p>
           <a
             href="https://github.com/ryan-zhu-music/ta-api"
-            className="bg-green-light text-green-dark text-center font-bold mt-5 px-6 py-3 rounded-full ring-2 ring-green-light shadow-[0px_0px_8px_3px_rgba(219,231,219,0.6)]"
+            className="bg-green-light text-green-dark text-center font-bold mt-5 px-6 py-3 rounded-full ring-2 ring-green-light shadow-[0px_0px_8px_3px_rgba(219,231,219,0.6)] hover:scale-105 duration-300 ease-in-out hover:shadow-[0px_0px_20px_3px_rgba(219,231,219,0.7)]"
             rel="noreferrer noopener"
           >
             View source code (GitHub)
@@ -108,19 +127,19 @@ export default function Home() {
         <section className="w-full flex flex-col items-center mt-20">
           <h2 className="text-3xl font-bold text-white">Usage</h2>
           <h3 className="text-xl font-bold text-white mt-5">Website</h3>
-          <p className="text-base text-center font-normal text-white mt-5">
-            Enter your YRDSB username and password above, and hit Get marks!
+          <p className="text-base text-center font-normal text-white mt-2">
+            Enter your YRDSB username and password above, and hit "Get Marks!""
             When the data is returned, it will be displayed below, along with
             the Download as JSON button.
           </p>
-          <h3 className="text-xl font-bold text-white mt-5">Request</h3>
-          <p className="text-base text-center font-normal text-white mt-5">
+          <h3 className="text-xl font-bold text-white mt-7">Request</h3>
+          <p className="text-base text-center font-normal text-white mt-2">
             Make a POST request to <code>/api/getCourses</code>, with a JSON
             body of two string parameters, <code>username</code> and{" "}
             <code>password</code>. Node fetch example provided below:
           </p>
-          <div className="w-full p-2 mt-5 ring-2 ring-green-light shadow-[0px_0px_8px_3px_rgba(219,231,219,0.6)] bg-green-transparent rounded-3xl">
-            <div className="w-full max-h-60 overflow-y-scroll overflow-x-hidden px-5 py-3">
+          <div className="w-full p-2 mt-7 ring-2 ring-green-light shadow-[0px_0px_8px_3px_rgba(219,231,219,0.6)] bg-green-transparent rounded-3xl">
+            <div className="w-full max-h-64 overflow-y-scroll overflow-x-hidden px-5 py-3">
               <pre className="text-white text-sm font-normal w-full">
                 {`const fetch = require('node-fetch');
 
@@ -136,167 +155,170 @@ fetch('https://ta-api.vercel.app/api/getCourses', {
               </pre>
             </div>
           </div>
-          <h3 className="text-xl font-bold text-white mt-5">Response</h3>
-          <p className="text-base text-center font-normal text-white mt-5">
+          <h3 className="text-xl font-bold text-white mt-10">Response</h3>
+          <p className="text-base text-center font-normal text-white mt-2">
             The response will be a JSON array of objects, one for each course.
-            The objects will have the following properties:
           </p>
-          <table className="w-full mt-5">
-            <thead>
+          <table className="w-full mt-7">
+            <thead className="text-md">
               <tr>
-                <th className="text-center text-white">Property</th>
-                <th className="text-center text-white">Type</th>
-                <th className="text-center text-white">Description</th>
-                <th className="text-center text-white">Example</th>
+                <th className="pb-2 text-center text-white">Property</th>
+                <th className="pb-2 text-center text-white">Type</th>
+                <th className="pb-2 text-center text-white">Description</th>
+                <th className="pb-2 text-center text-white">Example</th>
               </tr>
             </thead>
-            <tbody>
+            <tbody className="text-xs">
               <tr>
-                <td className="text-center">
+                <td className="px-2 text-center">
                   <code>code</code>
                 </td>
-                <td className="text-center text-emerald-200/80">
+                <td className="px-2 text-center text-emerald-200/80">
                   <code>string</code>
                 </td>
-                <td className="text-left text-white">Course code</td>
-                <td className="text-center">
+                <td className="px-2 text-center text-white">Course code</td>
+                <td className="px-2 text-center">
                   <code>&quot;ICS4U1-03&quot;</code>
                 </td>
               </tr>
               <tr>
-                <td className="text-center">
+                <td className="px-2 text-center">
                   <code>name</code>
                 </td>
-                <td className="text-center text-emerald-200/80">
+                <td className="px-2 text-center text-emerald-200/80">
                   <code>string</code>
                 </td>
-                <td className="text-left text-white">Course name</td>
-                <td className="text-center">
+                <td className="px-2 text-center text-white">Course name</td>
+                <td className="px-2 text-center">
                   <code>&quot;Computer and Information Science&quot;</code>
                 </td>
               </tr>
               <tr>
-                <td className="text-center">
+                <td className="px-2 text-center">
                   <code>block</code>
                 </td>
-                <td className="text-center text-emerald-200/80">
+                <td className="px-2 text-center text-emerald-200/80">
                   <code>string</code>
                 </td>
-                <td className="text-left text-white">Block/period</td>
-                <td className="text-center">
+                <td className="px-2 text-center text-white">Block/period</td>
+                <td className="px-2 text-center">
                   <code>&quot;2&quot;</code>
                 </td>
               </tr>
               <tr>
-                <td className="text-center">
+                <td className="px-2 text-center">
                   <code>room</code>
                 </td>
-                <td className="text-center text-emerald-200/80">
+                <td className="px-2 text-center text-emerald-200/80">
                   <code>string</code>
                 </td>
-                <td className="text-left text-white">Room number</td>
-                <td className="text-center">
+                <td className="px-2 text-center text-white">Room number</td>
+                <td className="px-2 text-center">
                   <code>&quot;208&quot;</code>
                 </td>
               </tr>
               <tr>
-                <td className="text-center">
+                <td className="px-2 text-center">
                   <code>start_time</code>
                 </td>
-                <td className="text-center text-emerald-200/80">
+                <td className="px-2 text-center text-emerald-200/80">
                   <code>string</code>
                 </td>
-                <td className="text-left text-white">Start time</td>
-                <td className="text-center">
+                <td className="px-2 text-center text-white">
+                  Course start date (yyyy-mm-dd)
+                </td>
+                <td className="px-2 text-center">
                   <code>&quot;2023-02-06&quot;</code>
                 </td>
               </tr>
               <tr>
-                <td className="text-center">
+                <td className="px-2 text-center">
                   <code>end_time</code>
                 </td>
-                <td className="text-center text-emerald-200/80">
+                <td className="px-2 text-center text-emerald-200/80">
                   <code>string</code>
                 </td>
-                <td className="text-left text-white">End time</td>
-                <td className="text-center">
+                <td className="px-2 text-center text-white">
+                  Course end date (yyyy-mm-dd)
+                </td>
+                <td className="px-2 text-center">
                   <code>&quot;2023-07-01&quot;</code>
                 </td>
               </tr>
               <tr>
-                <td className="text-center">
+                <td className="px-2 text-center">
                   <code>overall_mark</code>
                 </td>
-                <td className="text-center text-emerald-200/80">
+                <td className="px-2 text-center text-emerald-200/80">
                   <code>number | string</code>
                 </td>
-                <td className="text-left text-white">Overall mark</td>
-                <td className="text-center">
+                <td className="px-2 text-center text-white">Overall mark</td>
+                <td className="px-2 text-center">
                   <code>99.4 | &quot;N/A&quot;</code>
                 </td>
               </tr>
               <tr>
-                <td className="text-center">
+                <td className="px-2 text-center">
                   <code>isFinal</code>
                 </td>
-                <td className="text-center text-emerald-200/80">
+                <td className="px-2 text-center text-emerald-200/80">
                   <code>boolean</code>
                 </td>
-                <td className="text-left text-white">
+                <td className="px-2 text-center text-white">
                   If the mark is a final mark
                 </td>
-                <td className="text-center">
+                <td className="px-2 text-center">
                   <code>true | false</code>
                 </td>
               </tr>
               <tr>
-                <td className="text-center">
+                <td className="px-2 text-center">
                   <code>isMidterm</code>
                 </td>
-                <td className="text-center text-emerald-200/80">
+                <td className="px-2 text-center text-emerald-200/80">
                   <code>boolean</code>
                 </td>
-                <td className="text-left text-white">
+                <td className="px-2 text-center text-white">
                   If the mark is a midterm mark
                 </td>
-                <td className="text-center">
+                <td className="px-2 text-center">
                   <code>true | false</code>
                 </td>
               </tr>
               <tr>
-                <td className="text-center">
+                <td className="px-2 text-center">
                   <code>assignments</code>
                 </td>
-                <td className="text-center text-emerald-200/80">
+                <td className="px-2 text-center text-emerald-200/80">
                   <code>Array</code>
                 </td>
-                <td className="text-left text-white">
+                <td className="px-2 text-center text-white">
                   Array of assignment objects
                 </td>
-                <td className="text-center">
+                <td className="px-2 text-center">
                   <code>[]</code>
                 </td>
               </tr>
               <tr>
-                <td className="text-center">
+                <td className="px-2 text-center">
                   <code>weight_table</code>
                 </td>
-                <td className="text-center text-emerald-200/80">
+                <td className="px-2 text-center text-emerald-200/80">
                   <code>Object</code>
                 </td>
-                <td className="text-left text-white">
+                <td className="px-2 text-center text-white">
                   Course weightings for each category (KU/T/C/A/F/O)
                 </td>
-                <td className="text-center">
+                <td className="px-2 text-center">
                   <code>{"{}"}</code>
                 </td>
               </tr>
             </tbody>
           </table>
-          <h4 className="text-xl text-center font-bold text-white mt-5">
+          <h4 className="text-md text-center font-bold text-white mt-5">
             Abbreviations
           </h4>
-          <ul className="list-disc list-inside text-white">
+          <ul className="list-disc list-inside text-white text-xs">
             <li>
               <code>KU</code> - Knowledge/Understanding
             </li>
@@ -326,6 +348,23 @@ fetch('https://ta-api.vercel.app/api/getCourses', {
             </li>
           </ul>
         </section>
+        <footer className="flex flex-col items-center justify-center w-full h-24 mt-14 border-t border-emerald-200/20">
+          <p className="text-xs text-white">
+            Made with{" "}
+            <span role="img" aria-label="love">
+              ❤️
+            </span>{" "}
+            by{" "}
+            <a
+              href="https://www.ryanzhu.com"
+              target="_blank"
+              rel="noreferrer noopener"
+              className="text-emerald-200/80 hover:text-emerald-200/100"
+            >
+              Ryan Zhu
+            </a>
+          </p>
+        </footer>
       </main>
     </>
   );
